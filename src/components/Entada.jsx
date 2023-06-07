8f31fe2684d1616bdd46a8b3afe97f31feb0bcdc
Álvaryxs <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2023-06-07 16:23:02 -0300

    Add files via upload

--- src/components/Entada.jsx
+++ src/components/Entada.jsx
@@ -1,17 +1,15 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
-function Entrada (){
-    return (
-        <div>
-            <section>
-                <div>
-<h1>
-    opoppopopoppo
-</h1>
-                <Button variant="light">Acessar</Button>
-                </div>
-            </section>
-            </div>
-    )
+function Entrada() {
+  return (
+    <div>
+      <section>
+        <div className="banner1">
+          <h1>Sistema para controle que otimização a gestão da sua empresa</h1>
+          <Button className="botaob1" variant="light">Acessar</Button>
+        </div>
+      </section>
+    </div>
+  );
 }
-export default Entrada
+export default Entrada;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Entrada from "../components/Entada";
 import Navbary from "../components/Navbar";
-
+import Sessao1 from "../components/sessao1";
 function Home(){
     return(
         <div>
             <Navbary/>
             <Entrada/>
+            <Sessao1/>
         </div>
     )
 }
