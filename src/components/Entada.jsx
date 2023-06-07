b2165ba2cc19c1eb28e06ae111e697c85e4f93f1
Álvaryxs <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2023-06-07 11:27:53 -0300

    Add files via upload

---
+++ src/components/Entada.jsx
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+
+function Entrada (){
+    return (
+        <div>
+            <section>
+                <div>
+<h1>
+    opoppopopoppo
+</h1>
+                <Button variant="light">Acessar</Button>
+                </div>
+            </section>
+            </div>
+    )
+}
+export default Entrada
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Entrada from "../components/Entada";
+import Navbary from "../components/Navbar";
+
+function Home(){
+    return(
+        <div>
+            <Navbary/>
+            <Entrada/>
+        </div>
+    )
+}
+
+export default Home
